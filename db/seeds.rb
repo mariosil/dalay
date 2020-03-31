c4c80e6d4ec6cd446a4f80be0b7c3a4b2eaa080a
mariosil <silveira.mario.lab@gmail.com>
2020-03-30 20:41:25 -0600

    [DALAY-SEEDS] Seeds script now creates soups and salad dishes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -120,3 +120,87 @@ dishes.each_with_index do |dish, index|
   photo = File.open(Rails.root.join('db', 'seeds_imgs', "entree_#{index}.jpeg"))
   dish.photo.attach(io: photo, filename: File.basename(photo))
 end
+
+puts 'Generating soups & salads Dish payloads'
+ss_group = dish_groups.shift
+ss_dish_payloads = [
+  {
+    restaurant: restaurants[0],
+    dish_group: ss_group,
+    name: 'Mandarin Broccoli Salad',
+    description: 'Sweet, crunchy and colorful, this salad has a fresh mix of textures and flavors.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[0],
+    dish_group: ss_group,
+    name: 'Pea and ham soup',
+    description: 'An absolute classic, made with just five ingredients. Serve with crusty bread for a nourishing, colourful lunch.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[1],
+    dish_group: ss_group,
+    name: 'Shaved Fennel Salad',
+    description: ' It has an incredible crunch thanks to the cucumbers, radishes and apples. And the finish of fennel fronds adds just the faintest hint of licorice flavor.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[1],
+    dish_group: ss_group,
+    name: 'Green lentil harissa soup',
+    description: 'Nourishing and delicious, this soup comes stuffed with celery, parsnips, leek, kale and chickpeas.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[2],
+    dish_group: ss_group,
+    name: 'Apple Sausage Salad with Cinnamon Vinaigrette',
+    description: 'Making croutons with cinnamon-raisin bread is sweet genius. Toss together the rest of the salad while they toast.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[2],
+    dish_group: ss_group,
+    name: 'Caldo verde (kale, chorizo and potato soup)',
+    description: 'Make kale the star of this nutritious, low calorie potato and chorizo soup.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[3],
+    dish_group: ss_group,
+    name: 'Cashew-Pear Tossed Salad',
+    description: 'The unexpected sweet-salty mix and lovely dressing make it a hit with everyone.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[3],
+    dish_group: ss_group,
+    name: 'Creamy pumpkin soup with blue cheese and sage toasties',
+    description: 'Served in big mugs with fat fingers of melty cheese toasties for dipping.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[0],
+    dish_group: ss_group,
+    name: 'Grilled Apple Tossed Salad',
+    description: 'The grilled apples in this salad combine so well with the blue cheese, walnuts and balsamic dressing.',
+    price: fake_price(range: 10..25)
+  },
+  {
+    restaurant: restaurants[1],
+    dish_group: ss_group,
+    name: 'French onion soup',
+    description: 'This deep beefy onion soup is topped with bubbling cheese melts, perfect for a comforting winter warmer.',
+    price: fake_price(range: 10..25)
+  }
+]
+
+puts 'Creating soups & salads Dishes'
+dishes = Dish.create(ss_dish_payloads)
+
+puts 'Attaching soups & salads Dishes photos'
+dishes.each_with_index do |dish, index|
+  photo = File.open(Rails.root.join('db', 'seeds_imgs', "ss_#{index}.jpg"))
+  dish.photo.attach(io: photo, filename: File.basename(photo))
+end
